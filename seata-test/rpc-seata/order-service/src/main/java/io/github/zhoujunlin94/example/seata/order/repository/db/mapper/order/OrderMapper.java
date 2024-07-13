7f3b6bfecd283a1a2226891c2496c08a20c44fac
zhoujunlin <zhoujunlin.work@outlook.com>
2024-07-13 21:24:18 +0800

    remove

--- seata-test/rpc-seata/order-service/src/main/java/io/github/zhoujunlin94/example/seata/order/repository/db/mapper/order/OrderMapper.java
+++
@@ -1,9 +0,0 @@
-package io.github.zhoujunlin94.example.seata.order.repository.db.mapper.order;
-
-import io.github.zhoujunlin94.example.seata.order.repository.db.entity.order.Order;
-import io.github.zhoujunlin94.meet.tk_mybatis.mapper.TKMapper;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface OrderMapper extends TKMapper<Order> {
-}
