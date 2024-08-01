846645fcdad917591804f8be9078c81c87baab45
zhoujunlin <zhoujunlin.work@outlook.com>
2024-08-01 22:09:46 +0800

    mqtt

---
+++ mqtt_test/src/main/java/io/github/zhoujunlin94/mqtt/test/config/MqttConsumer2.java
@@ -0,0 +1,55 @@
+package io.github.zhoujunlin94.mqtt.test.config;
+
+import cn.hutool.core.util.IdUtil;
+import lombok.SneakyThrows;
+import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+/**
+ * @author zhoujunlin
+ * @date 2024/8/1 21:34
+ */
+public class MqttConsumer2 {
+
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        try {
+            MqttClient client = new MqttClient("tcp://broker.emqx.io:1883", IdUtil.fastSimpleUUID(), new MemoryPersistence());
+            MqttConnectOptions options = new MqttConnectOptions();
+            options.setCleanSession(false);
+            options.setKeepAliveInterval(60);
+            options.setConnectionTimeout(60);
+            options.setAutomaticReconnect(true);
+            // 设置回调
+            client.setCallback(new MqttCallback() {
+
+                @Override
+                public void connectionLost(Throwable cause) {
+                    System.out.println("connectionLost: " + cause.getMessage());
+                }
+
+                @Override
+                public void messageArrived(String topic, MqttMessage message) {
+                    System.out.println("topic: " + topic);
+                    System.out.println("Qos: " + message.getQos());
+                    System.out.println("message content: " + new String(message.getPayload()));
+
+                }
+
+                @Override
+                public void deliveryComplete(IMqttDeliveryToken token) {
+                    System.out.println("deliveryComplete---------" + token.isComplete());
+                }
+
+            });
+
+            client.connect(options);
+            client.subscribe("$share/group1/mqtt/test", 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
---
+++ mqtt_test/src/main/java/io/github/zhoujunlin94/mqtt/test/config/MqttPublisher.java
@@ -0,0 +1,44 @@
+package io.github.zhoujunlin94.mqtt.test.config;
+
+import cn.hutool.core.util.IdUtil;
+import com.alibaba.fastjson.JSONObject;
+import lombok.SneakyThrows;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author zhoujunlin
+ * @date 2024/8/1 21:34
+ */
+public class MqttPublisher {
+
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        MqttClient client = new MqttClient("tcp://broker.emqx.io:1883", "b25b0d43dd134a52b4e1fedb486c335a", new MemoryPersistence());
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setCleanSession(false);
+        options.setKeepAliveInterval(60);
+        options.setConnectionTimeout(60);
+        options.setAutomaticReconnect(true);
+        client.connect(options);
+
+        JSONObject content = new JSONObject().fluentPut("msg", "every body here?").fluentPut("msgId", IdUtil.fastSimpleUUID());
+        // 创建消息并设置 QoS
+        MqttMessage message = new MqttMessage(content.toJSONString().getBytes(StandardCharsets.UTF_8));
+        message.setQos(0);
+
+        // 发布消息
+        client.publish("mqtt/test", message);
+        // 关闭连接
+        client.disconnect();
+        // 关闭客户端
+        client.close();
+    }
+
+
+}
